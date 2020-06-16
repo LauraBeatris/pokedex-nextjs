1547ddf81c511ed330ef1bb4ad046b640901d4ee
Laura <laurabeatriserafim@gmail.com>
2020-06-16 08:28:41 -0300

    feat: add api route to search pokemons

---
+++ pages/api/search.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import pokemonData from 'pokemon.json';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const filter = req.query.search ?
+    new RegExp(req.query.search as string, "i")
+    :
+    /.*/;
+
+  const filteredPokemons = pokemonData
+    .filter(({ name: { english } }) => english.match(filter))
+    .slice(0, 10);
+
+  res.setHeader("Content-Type", "application/json");
+
+  return res.
+    status(200)
+    .send(JSON.stringify(filteredPokemons))
+}
