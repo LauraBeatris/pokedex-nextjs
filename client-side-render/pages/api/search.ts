dc72009a3b533507a468147068035528f1f0d3b1
Laura <laurabeatriserafim@gmail.com>
2020-07-02 09:16:39 -0300

    perf: improve search pokemon api route

--- client-side-render/pages/api/search.ts
+++ client-side-render/pages/api/search.ts
@@ -1,15 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import pokemonData from "pokemon.json";
+import getPokemonImage from "utils/getPokemonImage"
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-  const filter = req.query.name
+  const filterQueryName = req.query.name
     ? new RegExp(req.query.name as string, "i")
     : /.*/;
 
   const filteredPokemons = pokemonData
-    .filter(({ name: { english } }) => english.match(filter))
-    .slice(0, 10);
+    .filter((pokemon) =>
+      pokemon.name.english.match(filterQueryName)
+    )
+    .slice(0, 10)
+    .map((pokemon) => {
+      return {
+        ...pokemon,
+        image: getPokemonImage(pokemon.name.english),
+        name: pokemon.name.english,
+      };
+    });
 
   res.setHeader("Content-Type", "application/json");
 
